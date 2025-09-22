23349f4ee432606b704734e0b90a0655a1e0973e
Alex Chitham <alexchitham@hotmail.co.uk>
2025-09-22 21:41:57 +0100

    Using a SQLite3 database for storage instead of a dataframe

--- src/main.py
+++ src/main.py
@@ -3,9 +3,10 @@ from fastapi import FastAPI, UploadFile, Path, HTTPException
 from pydantic import BaseModel
 from typing import Annotated
 
-import pandas as pd
-import numpy as np
+import sqlite3
+import csv
 from io import StringIO
+import os
 
 
 # Initialise the app
@@ -17,30 +18,66 @@ class Statistics(BaseModel):
     minimum: float
     mean: float
 
-# Define state attribute to keep the CSV in memory after being uploaded
-app.state.csv_data = None
 
 # Endpoint for uploading the CSV file
 @app.post("/upload", summary="Path operation for uploading the CSV file")
 async def csv_upload(file: UploadFile):
 
     # Ensure the file is the correct type
-    if file.filename.endswith(".csv"):
-
-        # Read the contents of the file in binary
-        contents = await file.read()
-        
-        # Decode the binary data to a string
-        string_contents = contents.decode("utf-8")
-
-        # Read the string data into a CSV dataframe and store in app state
-        app.state.csv_data = pd.read_csv(StringIO(string_contents))
-
-        # Success message
-        return {"message": f"{file.filename} uploaded successfully"}
-
-    else: 
+    if not file.filename.endswith(".csv"):
         raise HTTPException(status_code=400, detail="Uploaded file is not a CSV file")
+
+    # Read the contents of the file in binary
+    contents = await file.read()
+
+    try:
+        # Decode the binary data
+        string_contents = StringIO(contents.decode("utf-8"))
+
+        # Read the file as CSV and temporarily store in memory
+        csv_reader = csv.DictReader(string_contents)
+        transactions = [
+            (row["transaction_id"], int(row["user_id"]), int(row["product_id"]), row["timestamp"], float(row["transaction_amount"]))
+            for row in csv_reader
+        ]
+
+        # Connect to SQLite database and create a cursor
+        sqliteConnection = sqlite3.connect("dummy_transactions.db")
+        cursor = sqliteConnection.cursor()
+
+        # Create the table
+        cursor.execute("""CREATE TABLE IF NOT EXISTS dummy_transactions (transaction_id TEXT, user_id INTEGER, 
+                       product_id INTEGER, timestamp TEXT, transaction_amount REAL);""")
+        
+        # Insert all the rows into the table
+        cursor.executemany("""INSERT INTO dummy_transactions (transaction_id, user_id, product_id, timestamp, 
+                           transaction_amount) VALUES (?, ?, ?, ?, ?);""", transactions)
+        
+        # Commit changes to the database
+        sqliteConnection.commit()
+                                                                         
+
+    # Catching all errors
+    except UnicodeDecodeError:
+        raise HTTPException(status_code=400, detail="Decoding error: File encoding is not UTF-8")
+    
+    except sqlite3.Error:
+        raise HTTPException(status_code=400, detail="Error with the internal database")
+    
+    except KeyError:
+        raise HTTPException(status_code=400, detail="Table is missing some of the required rows")
+    
+    # Close the cursor and database
+    finally:
+        if cursor:
+            cursor.close()   
+        if sqliteConnection:
+            sqliteConnection.close()
+  
+
+    # Success message
+    return {"message": f"{file.filename} uploaded successfully"}
+        
 
 
     
@@ -49,22 +86,37 @@ async def csv_upload(file: UploadFile):
 async def get_stats(user_id: Annotated[int, Path(title="The ID of the user to calculate stats for")]) -> Statistics:
 
     # Check a CSV file has been uploaded before calling this endpoint
-    if app.state.csv_data is not None:
+    if not os.path.exists("dummy_transactions.db"):
+        raise HTTPException(status_code=400, detail="CSV file has not been uploaded") 
 
-        csv_data = app.state.csv_data
+    try:
+        # Connect to SQLite database and create a cursor
+        sqliteConnection = sqlite3.connect("dummy_transactions.db")
+        cursor = sqliteConnection.cursor()
 
-        # Check if the given user ID exists in the CSV file
-        if not (csv_data["user_id"] == user_id).any():
+        # Query the database, finding the max, min and mean simultaneously
+        cursor.execute("""SELECT MAX(transaction_amount), MIN(transaction_amount), AVG(transaction_amount) 
+                       FROM dummy_transactions WHERE user_id = ?""", (user_id,))
+        
+        # Store the queried results in new variables
+        (max, min, mean) = cursor.fetchone()
+
+        # Check if the user ID was found in the file
+        if max is None:
             raise HTTPException(status_code=404, detail="User ID not found in CSV file")
 
-        # Extract the transaction amounts for the user with the given user ID
-        user_transactions = csv_data[csv_data["user_id"] == user_id]
-        amounts = (user_transactions["transaction_amount"]).to_numpy()
+    # Catching database error
+    except sqlite3.Error:
+        raise HTTPException(status_code=400, detail="Error with the internal database")
+    
+    # Close the cursor and database
+    finally:
+        if cursor:
+            cursor.close()   
+        if sqliteConnection:
+            sqliteConnection.close()
 
-        # Create a Statistics item to return, using the max, min, and mean
-        stats = Statistics(maximum=np.max(amounts), minimum=np.min(amounts), mean=np.mean(amounts))
-
-    else:
-        raise HTTPException(status_code=400, detail="CSV file has not been uploaded") 
+    # Create a Statistics item to return, using the max, min, and mean
+    stats = Statistics(maximum=max, minimum=min, mean=mean)
 
     return stats
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,7 @@
 
 from fastapi.testclient import TestClient
 from main import app
-import pandas as pd
+import os
 
 
 # Initialise the client to test the app
@@ -10,7 +10,9 @@ client = TestClient(app)
 # Function to test uploading a file of the incorrect type
 def test_invalid_upload():
 
-    app.state.csv_data = None
+    if os.path.exists("dummy_transactions.db"):
+        os.remove("dummy_transactions.db")
+
     with open("README.md", "rb") as md_file:
         files = {"file": md_file}
         response = client.post("/upload", files=files)
@@ -18,11 +20,16 @@ def test_invalid_upload():
     assert response.status_code == 400
     assert response.json() == {"detail": "Uploaded file is not a CSV file"}
 
+    if os.path.exists("dummy_transactions.db"):
+        os.remove("dummy_transactions.db")
+
 
 # Function to test uploading a file of the correct type
 def test_valid_upload():
     
-    app.state.csv_data = None
+    if os.path.exists("dummy_transactions.db"):
+        os.remove("dummy_transactions.db")
+
     with open("dummy_transactions.csv", "rb") as csv_file:
         files = {"file": csv_file}
         response = client.post("/upload", files=files)
@@ -30,20 +37,30 @@ def test_valid_upload():
     assert response.status_code == 200
     assert response.json() == {"message": "dummy_transactions.csv uploaded successfully"}
 
+    if os.path.exists("dummy_transactions.db"):
+        os.remove("dummy_transactions.db")
+
 
 # Function to test response if querying the data before uploading a CSV file
 def test_get_stats_early_call():
     
-    app.state.csv_data = None
+    if os.path.exists("dummy_transactions.db"):
+        os.remove("dummy_transactions.db")
+
     response = client.get("/summary/1")
     assert response.status_code == 400
     assert response.json() == {"detail": "CSV file has not been uploaded"}
+
+    if os.path.exists("dummy_transactions.db"):
+        os.remove("dummy_transactions.db")
     
 
 # Function to test passing in an invalid user ID
 def test_get_stats_invalid_user():
 
-    app.state.csv_data = None
+    if os.path.exists("dummy_transactions.db"):
+        os.remove("dummy_transactions.db")
+
     with open("dummy_transactions.csv", "rb") as csv_file:
         files = {"file": csv_file}
         client.post("/upload", files=files)
@@ -64,11 +81,16 @@ def test_get_stats_invalid_user():
     assert response.status_code == 422
     assert response.json()["detail"][0]["msg"] == "Input should be a valid integer, unable to parse string as an integer"
 
+    if os.path.exists("dummy_transactions.db"):
+        os.remove("dummy_transactions.db")
+
 
 # Function to test the summary statistics calculations on test CSV file
 def test_valid_get_stats():
 
-    app.state.csv_data = None
+    if os.path.exists("dummy_transactions.db"):
+        os.remove("dummy_transactions.db")
+
     with open("tests/test.csv", "rb") as csv_file:
         files = {"file": csv_file}
         response = client.post("/upload", files=files)
@@ -104,7 +126,5 @@ def test_valid_get_stats():
     assert response.status_code == 404
     assert response.json() == {"detail": "User ID not found in CSV file"}
 
-
-                            
-
-    
+    if os.path.exists("dummy_transactions.db"):
+        os.remove("dummy_transactions.db")
